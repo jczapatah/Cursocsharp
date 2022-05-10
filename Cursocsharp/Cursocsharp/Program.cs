606e6fa2d61014379522e425a66db80f73fe59a5
jczapatah <jczapatah28@gmail.com>
2022-05-10 18:18:39 +0200

    Finish Date Class

---
+++ Cursocsharp/Cursocsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cursocsharp;
+
+Console.WriteLine("Hello, World!");
+
+//Date date1 = new Date(2022, 05, 10);
+
+try
+{
+    Console.WriteLine(new Date(2024, 02, 29));
+    Console.WriteLine(new Date(2020, 11, 21));
+    Console.WriteLine(new Date(1983, 11, 30));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
